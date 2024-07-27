4ad1b2eace082b9c41e7a7e801f26a0fa1a5ef50
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-07-27 17:16:15 +0200

    Update gisele_working.py

--- gisele_working.py
+++ gisele_working.py
@@ -259,6 +259,7 @@ elif main_nav == "Data Collection":
                     google_buildings['source'] = 'google'
     
                     combined_buildings = create_combined_buildings_layer(osm_buildings, google_buildings)
+                    st.DataFrame(combined_buildings)
                     create_map(latitude, longitude, combined_buildings=combined_buildings)
                 except Exception as e:
                     st.error(f"Error fetching OSM buildings data: {e}")
