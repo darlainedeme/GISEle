929be19e52a53153c5553218388478cc0b0dc6a2
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-07-27 14:06:49 +0200

    Update gisele.py

--- gisele.py
+++ gisele.py
@@ -26,8 +26,8 @@ def initialize_earth_engine():
     ee.Initialize(credentials)
 
 # Ensure Earth Engine is initialized only once
+@st.cache_resource
 if 'ee_initialized' not in st.session_state:
-    @st.cache_resource
     initialize_earth_engine()
     st.session_state.ee_initialized = True
 # Define navigation
