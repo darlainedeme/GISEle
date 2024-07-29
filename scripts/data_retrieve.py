e6ef6603defef24190ac391b90787c21088d1ab7
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-07-29 14:26:32 +0200

    Update data_retrieve.py

--- scripts/data_retrieve.py
+++ scripts/data_retrieve.py
@@ -5,6 +5,7 @@ import geopandas as gpd
 import json
 from scripts.utils import initialize_earth_engine, create_combined_buildings_layer
 import zipfile
+import os
 
 def download_osm_data(polygon, tags, file_path):
     data = ox.geometries_from_polygon(polygon, tags)
