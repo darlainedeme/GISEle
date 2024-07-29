8273cb8966c23f2da31711a7e7166eb510ceab55
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-07-29 13:33:52 +0200

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,122 +7,13 @@ import ee
 from folium.plugins import MarkerCluster
 import streamlit as st
 
-# Initialize Earth Engineimport folium
-from streamlit_folium import st_folium
-import geopandas as gpd
-import json
-import tempfile
-import ee
-from folium.plugins import MarkerCluster
-import streamlit as st
-
 # Initialize Earth Engine
 @st.cache_resource
 def initialize_earth_engine():
     json_data = st.secrets["json_data"]
-    json_object = json.loads(json_str, strict=False)
+    json_object = json.loads(json_data, strict=False)
     service_account = json_object['client_email']
-    credentials = ee.ServiceAccountCredentials(service_account, key_data=json_str)
-    ee.Initialize(credentials)
-
-def create_map(latitude, longitude, geojson_data=None, combined_buildings=None, osm_roads=None, osm_pois=None):
-    m = folium.Map(location=[latitude, longitude], zoom_start=15)
-
-    # Add map tiles
-    folium.TileLayer('cartodbpositron', name="Positron").add_to(m)
-    folium.TileLayer('cartodbdark_matter', name="Dark Matter").add_to(m)
-    folium.TileLayer(
-        tiles='http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}',
-        attr='Google',
-        name='Google Maps',
-        overlay=False,
-        control=True
-    ).add_to(m)
-    folium.TileLayer(
-        tiles='http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}',
-        attr='Google',
-        name='Google Hybrid',
-        overlay=False,
-        control=True
-    ).add_to(m)
-    folium.TileLayer(
-        tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-        attr='Esri',
-        name='Esri Satellite',
-        overlay=False,
-        control=True
-    ).add_to(m)
-
-    # Add the original area of interest
-    if geojson_data:
-        folium.GeoJson(
-            geojson_data,
-            name="Original Area",
-            style_function=lambda x: {
-                'fillColor': 'blue',
-                'color': 'blue',
-                'weight': 2,
-                'fillOpacity': 0.2,
-            }
-        ).add_to(m)
-
-    # Add combined buildings data to the map
-    if combined_buildings is not None:
-        combined_buildings_layer = folium.FeatureGroup(name="Combined Buildings").add_to(m)
-        style_function = lambda x: {
-            'fillColor': 'green' if x['properties']['source'] == 'osm' else 'blue',
-            'color': 'green' if x['properties']['source'] == 'osm' else 'blue',
-            'weight': 1,
-        }
-        folium.GeoJson(combined_buildings, name="Combined Buildings", style_function=style_function).add_to(combined_buildings_layer)
-
-        # Add MarkerCluster for combined buildings
-        marker_cluster = MarkerCluster(name='Combined Buildings Clusters').add_to(m)
-        for _, row in combined_buildings.iterrows():
-            folium.Marker(location=[row.geometry.centroid.y, row.geometry.centroid.x]).add_to(marker_cluster)
-
-    # Add OSM Roads data to the map
-    if osm_roads is not None:
-        folium.GeoJson(osm_roads.to_json(), name='OSM Roads', style_function=lambda x: {
-            'fillColor': 'orange',
-            'color': 'orange',
-            'weight': 1,
-        }).add_to(m)
-
-    # Add OSM Points of Interest data to the map
-    if osm_pois is not None:
-        folium.GeoJson(osm_pois.to_json(), name='OSM Points of Interest', style_function=lambda x: {
-            'fillColor': 'red',
-            'color': 'red',
-            'weight': 1,
-        }).add_to(m)
-
-    # Add drawing and fullscreen plugins
-    folium.plugins.Draw(export=True, filename='data.geojson', position='topleft').add_to(m)
-    folium.plugins.Fullscreen(position='topleft', title='Full Screen', title_cancel='Exit Full Screen',
-                              force_separate_button=False).add_to(m)
-    folium.plugins.MeasureControl(position='bottomleft', primary_length_unit='meters', secondary_length_unit='miles',
-                                  primary_area_unit='sqmeters', secondary_area_unit='acres').add_to(m)
-    folium.LayerControl().add_to(m)
-    
-    # Display the map
-    st_folium(m, width=1400, height=800)  # Wider map
-
-def uploaded_file_to_gdf(data):
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".geojson") as temp_file:
-        temp_file.write(data.getvalue())
-        temp_filepath = temp_file.name
-
-    gdf = gpd.read_file(temp_filepath)
-    return gdf
-
-@st.cache_resource
-def initialize_earth_engine():
-    json_data = st.secrets["json_data"]
-    json_str = json.dumps(json_data)
-    json_object = json.loads(json_str, strict=False)
-    service_account = json_object['client_email']
-    credentials = ee.ServiceAccountCredentials(service_account, key_data=json_str)
+    credentials = ee.ServiceAccountCredentials(service_account, key_data=json_data)
     ee.Initialize(credentials)
 
 def create_map(latitude, longitude, geojson_data=None, combined_buildings=None, osm_roads=None, osm_pois=None):
