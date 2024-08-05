d21b7556c6aee0dec85c667d30f7cf0019aabe04
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-05 16:07:15 +0200

    Update michele.py

--- scripts/routing_scripts/michele/michele.py
+++ scripts/routing_scripts/michele/michele.py
@@ -1,28 +1,27 @@
 from pyomo.environ import AbstractModel, Var, value
-
 from gisele.michele.components_creation import Model_Creation
-#from gisele.michele.model_solve import Model_Instance
 from gisele.michele.model_solve import Model_Resolution
 from gisele.michele.results import Load_results
 from gisele.michele.components_initialization import importing
 from pyomo.opt import SolverFactory
 import pandas as pd
+import numpy as np  # Add this import for numpy
 
 # Ensure that numpy floats are correctly referenced
 _floats = [np.float_, np.float16, np.float32, np.float64]
 
-def start(load_profile, pv_avg, wt_avg,input_michele, ht_avg,n_mg):
+def start(load_profile, pv_avg, wt_avg, input_michele, ht_avg, n_mg):
     input_load, wt_prod, pv_prod = importing(load_profile, pv_avg, wt_avg, ht_avg)
 
-    model = AbstractModel()  # define type of optimization problem
+    model = AbstractModel()  # Define type of optimization problem
 
     # Optimization model
     print('Starting model creation')
-    Model_Creation(model, input_load, wt_prod, pv_prod,input_michele)  # Creation of the Sets, parameters and variables.
+    Model_Creation(model, input_load, wt_prod, pv_prod, input_michele)  # Creation of the Sets, parameters and variables.
     print('Starting model resolution')
 
     instance = model.create_instance(
-        r'gisele\michele\Inputs\data.json')  # load parameters
+        r'gisele\michele\Inputs\data.json')  # Load parameters
 
     # opt = SolverFactory('cplex',executable=r'C:\Program Files\IBM\ILOG\CPLEX_Studio1210\cplex\bin\x64_win64\cplex')
     opt = SolverFactory('gurobi')  # Solver use during the optimization
@@ -30,27 +29,23 @@ def start(load_profile, pv_avg, wt_avg,input_michele, ht_avg,n_mg):
     print('Begin Optimization')
     result_michele = {}
 
-    if n_mg==1:
-
-        print('Solving for minimum renewable fraction of '+str(instance.ren_fraction.extract_values()[None]*100)+'%')
+    if n_mg == 1:
+        print('Solving for minimum renewable fraction of ' + str(instance.ren_fraction.extract_values()[None] * 100) + '%')
         results = opt.solve(instance, tee=True)  # Solving a model instance
         instance.solutions.load_from(results)  # Loading solution into instance
         result_michele['0'] = Load_results(instance)
 
     else:
-
-        print('Solving for '+str(n_mg)+' different values of minimum renewable fraction, ranging from 0% to 100%')
-
-        renfr=-1  # initialize at a value lower than 0
+        print('Solving for ' + str(n_mg) + ' different values of minimum renewable fraction, ranging from 0% to 100%')
+        renfr = -1  # Initialize at a value lower than 0
 
         for i in range(n_mg):
-            min_renfr = i*1/(n_mg-1)
-            instance.ren_fraction=min_renfr
-            print('Solving for minimum renewable fraction of '+str(min_renfr*100)+'%')
+            min_renfr = i * 1 / (n_mg - 1)
+            instance.ren_fraction = min_renfr
+            print('Solving for minimum renewable fraction of ' + str(min_renfr * 100) + '%')
 
             if renfr >= min_renfr:
                 print('Skipping solve as minimum renewable fraction already fulfilled')
-
             else:
                 results = opt.solve(instance, tee=True)  # Solving a model instance
                 instance.solutions.load_from(results)  # Loading solution into instance
@@ -61,13 +56,10 @@ def start(load_profile, pv_avg, wt_avg,input_michele, ht_avg,n_mg):
                 dg_tot = 0.001 * h_weight * sum(dg_power.iloc[h, 0] for h in range(project_duration))
                 load = pd.DataFrame.from_dict(instance.Load.get_values(), orient='index')
                 load_tot = 0.001 * h_weight * sum(load.iloc[h, 0] for h in range(project_duration))
-                renfr = 1-dg_tot/load_tot
-                print('Renewable fraction='+str(renfr*100)+'%')
+                renfr = 1 - dg_tot / load_tot
+                print('Renewable fraction=' + str(renfr * 100) + '%')
 
             print('Show results')
-            result_michele[str(i)]= Load_results(instance)
+            result_michele[str(i)] = Load_results(instance)
 
     return result_michele
-
-
-
