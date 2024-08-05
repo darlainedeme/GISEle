a5f51ee5584a71bdb6527464e7026b704a43fa51
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-05 18:49:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ st.set_page_config(layout="wide")
 
 from scripts import home, area_selection, data_retrieve, buildings, clustering, data_visualization, costs, summary_analysis, demand_estimation, routing, results
 
-'''
-import os
-from osgeo import gdal
-
-# Set GDAL environment variables
-os.environ['GDAL_DATA'] = '/usr/share/gdal'
-os.environ['PROJ_LIB'] = '/usr/share/proj'
-'''
-
 # Define navigation
 main_nav = st.sidebar.radio("Navigation", [
     "Home", "Area Selection", "Data Retrieve", "Buildings", 
@@ -22,33 +13,23 @@ main_nav = st.sidebar.radio("Navigation", [
 
 if main_nav == "Home":
     home.show()
-
 elif main_nav == "Area Selection":
     area_selection.show()
-
 elif main_nav == "Data Retrieve":
     data_retrieve.show()
-
 elif main_nav == "Buildings":
     buildings.show()
-
 elif main_nav == "Clustering":
     clustering.show()
-
 elif main_nav == "Data Visualization and Enhancement":
     data_visualization.show()
-
 elif main_nav == "Summary Analysis":
     summary_analysis.show()
-    
 elif main_nav == "Costs":
     costs.show()
-
 elif main_nav == "Demand Estimation":
     demand_estimation.show()
-
 elif main_nav == "Routing":
     routing.show()
-
 elif main_nav == "Results":
     results.show()
