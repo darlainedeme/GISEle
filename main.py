10686abcbe1699161bd30d1d2de89255b952e97e
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-06 23:18:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 import streamlit as st
 st.set_page_config(layout="wide")
 
-from scripts import home, area_selection, data_retrieve, buildings, clustering, data_visualization, costs, summary_analysis
+from scripts import home, area_selection, data_retrieve, buildings, clustering, data_visualization, costs, summary_analysis, demand_estimation, routing, results
 
 # Define navigation
-main_nav = st.sidebar.radio("Navigation", ["Home", "Area Selection", "Data Retrieve", "Buildings", "Clustering", "Data Visualization", "Costs", "Summary Analysis"])
+main_nav = st.sidebar.radio("Navigation", [
+    "Home", "Area Selection", "Data Retrieve", "Buildings", 
+    "Clustering", "Data Visualization and Enhancement", 
+    "Summary Analysis", "Costs", "Demand Estimation", 
+    "Routing", "Results"
+])
 
-# Navigation logic
 if main_nav == "Home":
     home.show()
 elif main_nav == "Area Selection":
@@ -23,9 +27,15 @@ elif main_nav == "Buildings":
     buildings.show()
 elif main_nav == "Clustering":
     clustering.show()
-elif main_nav == "Data Visualization":
+elif main_nav == "Data Visualization and Enhancement":
     data_visualization.show()
-elif main_nav == "Costs":
-    costs.show()
 elif main_nav == "Summary Analysis":
     summary_analysis.show()
+elif main_nav == "Costs":
+    costs.show()
+elif main_nav == "Demand Estimation":
+    demand_estimation.show()
+elif main_nav == "Routing":
+    routing.show()
+elif main_nav == "Results":
+    results.show()
