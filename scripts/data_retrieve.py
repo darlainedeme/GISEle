9267f6cb4ecf8d3f3815076ef5ea1d8a2d04d977
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-06 14:25:30 +0200

    Update data_retrieve.py

--- scripts/data_retrieve.py
+++ scripts/data_retrieve.py
@@ -144,8 +144,8 @@ def show():
                              
             if "Elevation" in selected_datasets:
                 status_text.text("Downloading elevation data...")
-                elevation_file = 'data/input/elevation/image_original.tif'
-                os.makedirs('data/input/elevation', exist_ok=True)
+                elevation_file = 'data/output/elevation/image_original.tif'
+                os.makedirs('data/output/elevation', exist_ok=True)
                 elevation_path = download_elevation_data(polygon, elevation_file)
 
                 if elevation_path:
