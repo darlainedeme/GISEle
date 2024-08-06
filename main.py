88f5effe7e970730f682a15bb07aee6879eab59b
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-06 23:18:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import sys
+import os
+
+# Add the project root directory to the Python path
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 import streamlit as st
 st.set_page_config(layout="wide")
 
-from scripts import home, area_selection, data_retrieve, buildings, clustering, data_visualization, costs, summary_analysis, demand_estimation, routing, results
+from scripts import home, area_selection, data_retrieve, buildings, clustering, data_visualization, costs, summary_analysis
 
 # Define navigation
-main_nav = st.sidebar.radio("Navigation", [
-    "Home", "Area Selection", "Data Retrieve", "Buildings", 
-    "Clustering", "Data Visualization and Enhancement", 
-    "Summary Analysis", "Costs", "Demand Estimation", 
-    "Routing", "Results"
-])
+main_nav = st.sidebar.radio("Navigation", ["Home", "Area Selection", "Data Retrieve", "Buildings", "Clustering", "Data Visualization", "Costs", "Summary Analysis"])
 
+# Navigation logic
 if main_nav == "Home":
     home.show()
 elif main_nav == "Area Selection":
@@ -21,15 +23,9 @@ elif main_nav == "Buildings":
     buildings.show()
 elif main_nav == "Clustering":
     clustering.show()
-elif main_nav == "Data Visualization and Enhancement":
+elif main_nav == "Data Visualization":
     data_visualization.show()
-elif main_nav == "Summary Analysis":
-    summary_analysis.show()
 elif main_nav == "Costs":
     costs.show()
-elif main_nav == "Demand Estimation":
-    demand_estimation.show()
-elif main_nav == "Routing":
-    routing.show()
-elif main_nav == "Results":
-    results.show()
+elif main_nav == "Summary Analysis":
+    summary_analysis.show()
