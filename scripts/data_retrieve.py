03edfc784668840466d0ccef19f6d0bf4cc8ab61
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-07 22:07:45 +0200

    Update data_retrieve.py

--- scripts/data_retrieve.py
+++ scripts/data_retrieve.py
@@ -72,108 +72,104 @@ def show():
         progress = st.progress(0)
         status_text = st.empty()
 
-        try:
-            if "Buildings" in selected_datasets:
-                status_text.text("Downloading buildings data...")
-                download_buildings_data(polygon)
-                progress.progress(0.1)
-                st.write("Buildings data downloaded.")
+        if "Buildings" in selected_datasets:
+            status_text.text("Downloading buildings data...")
+            download_buildings_data(polygon)
+            progress.progress(0.1)
+            st.write("Buildings data downloaded.")
 
-            if "Roads" in selected_datasets:
-                status_text.text("Downloading roads data...")
-                download_roads_data(buffer_polygon)
-                progress.progress(0.2)
-                st.write("Roads data downloaded.")
+        if "Roads" in selected_datasets:
+            status_text.text("Downloading roads data...")
+            download_roads_data(buffer_polygon)
+            progress.progress(0.2)
+            st.write("Roads data downloaded.")
 
-            if "Points of Interest" in selected_datasets:
-                status_text.text("Downloading points of interest data...")
-                download_poi_data(polygon)
-                progress.progress(0.3)
-                st.write("Points of interest data downloaded.")
+        if "Points of Interest" in selected_datasets:
+            status_text.text("Downloading points of interest data...")
+            download_poi_data(polygon)
+            progress.progress(0.3)
+            st.write("Points of interest data downloaded.")
 
-            if "Water Bodies" in selected_datasets:
-                status_text.text("Downloading water bodies data...")
-                download_water_bodies_data(polygon)
-                progress.progress(0.4)
-                st.write("Water bodies data downloaded.")
+        if "Water Bodies" in selected_datasets:
+            status_text.text("Downloading water bodies data...")
+            download_water_bodies_data(polygon)
+            progress.progress(0.4)
+            st.write("Water bodies data downloaded.")
 
-            if "Major Cities" in selected_datasets:
-                status_text.text("Downloading major cities data...")
-                download_cities_data(buffer_polygon)
-                progress.progress(0.5)
-                st.write("Major cities data downloaded.")
+        if "Major Cities" in selected_datasets:
+            status_text.text("Downloading major cities data...")
+            download_cities_data(buffer_polygon)
+            progress.progress(0.5)
+            st.write("Major cities data downloaded.")
 
-            if "Airports" in selected_datasets:
-                status_text.text("Downloading airports data...")
-                download_airports_data(buffer_polygon)
-                progress.progress(0.6)
-                st.write("Airports data downloaded.")
+        if "Airports" in selected_datasets:
+            status_text.text("Downloading airports data...")
+            download_airports_data(buffer_polygon)
+            progress.progress(0.6)
+            st.write("Airports data downloaded.")
 
-            if "Ports" in selected_datasets:
-                status_text.text("Downloading ports data...")
-                download_ports_data(buffer_polygon)
-                progress.progress(0.7)
-                st.write("Ports data downloaded.")
+        if "Ports" in selected_datasets:
+            status_text.text("Downloading ports data...")
+            download_ports_data(buffer_polygon)
+            progress.progress(0.7)
+            st.write("Ports data downloaded.")
 
-            if "Power Lines" in selected_datasets:
-                status_text.text("Downloading power lines data...")
-                download_power_lines_data(buffer_polygon)
-                progress.progress(0.8)
-                st.write("Power lines data downloaded.")
+        if "Power Lines" in selected_datasets:
+            status_text.text("Downloading power lines data...")
+            download_power_lines_data(buffer_polygon)
+            progress.progress(0.8)
+            st.write("Power lines data downloaded.")
 
-            if "Transformers and Substations" in selected_datasets:
-                status_text.text("Downloading transformers and substations data...")
-                download_substations_data(buffer_polygon)
-                progress.progress(0.9)
-                st.write("Transformers and substations data downloaded.")
-                
-            if "Elevation" in selected_datasets:
-                status_text.text("Downloading elevation data...")
-                download_elevation_data(polygon)
-                progress.progress(1.0)
-                st.write("Elevation data downloaded.")
-
-            if "Solar Potential" in selected_datasets:
-                status_text.text("Downloading solar data...")
-                download_solar_data(polygon)
-                progress.progress(1.1)
-                st.write("Solar data downloaded.")
-
-            if "Wind Potential" in selected_datasets:
-                status_text.text("Downloading wind data...")
-                download_wind_data(polygon)
-                progress.progress(1.2)
-                st.write("Wind data downloaded.")
-
-            if "Satellite" in selected_datasets:
-                status_text.text("Downloading satellite data...")
-                download_satellite_data(polygon)
-                progress.progress(1.3)
-                st.write("Satellite data downloaded.")
-
-            if "Nighttime Lights" in selected_datasets:
-                status_text.text("Downloading nighttime lights data...")
-                download_nighttime_lights_data(polygon)
-                progress.progress(1.4)
-                st.write("Nighttime lights data downloaded.")
-
-            if "Population" in selected_datasets:
-                status_text.text("Downloading population data...")
-                download_population_data('data/3_user_uploaded_data/selected_area.geojson', 2020)
-                progress.progress(1.5)
-                st.write("Population data downloaded.")
-
-            status_text.text("Zipping results...")
-            st.write(os.getcwd())
-            zip_results('data/2_downloaded_input_data', 'data/2_downloaded_input_data/results.zip')
+        if "Transformers and Substations" in selected_datasets:
+            status_text.text("Downloading transformers and substations data...")
+            download_substations_data(buffer_polygon)
+            progress.progress(0.9)
+            st.write("Transformers and substations data downloaded.")
+            
+        if "Elevation" in selected_datasets:
+            status_text.text("Downloading elevation data...")
+            download_elevation_data(polygon)
             progress.progress(1.0)
+            st.write("Elevation data downloaded.")
 
-            st.success("Data download complete. You can now proceed to the next section.")
-            with open('data/2_downloaded_input_data/results.zip', 'rb') as f:
-                st.download_button('Download All Results', f, file_name='results.zip')
+        if "Solar Potential" in selected_datasets:
+            status_text.text("Downloading solar data...")
+            download_solar_data(polygon)
+            progress.progress(1.1)
+            st.write("Solar data downloaded.")
 
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
+        if "Wind Potential" in selected_datasets:
+            status_text.text("Downloading wind data...")
+            download_wind_data(polygon)
+            progress.progress(1.2)
+            st.write("Wind data downloaded.")
+
+        if "Satellite" in selected_datasets:
+            status_text.text("Downloading satellite data...")
+            download_satellite_data(polygon)
+            progress.progress(1.3)
+            st.write("Satellite data downloaded.")
+
+        if "Nighttime Lights" in selected_datasets:
+            status_text.text("Downloading nighttime lights data...")
+            download_nighttime_lights_data(polygon)
+            progress.progress(1.4)
+            st.write("Nighttime lights data downloaded.")
+
+        if "Population" in selected_datasets:
+            status_text.text("Downloading population data...")
+            download_population_data('data/3_user_uploaded_data/selected_area.geojson', 2020)
+            progress.progress(1.5)
+            st.write("Population data downloaded.")
+
+        status_text.text("Zipping results...")
+        st.write(os.getcwd())
+        zip_results('data/2_downloaded_input_data', 'data/2_downloaded_input_data/results.zip')
+        progress.progress(1.0)
+
+        st.success("Data download complete. You can now proceed to the next section.")
+        with open('data/2_downloaded_input_data/results.zip', 'rb') as f:
+            st.download_button('Download All Results', f, file_name='results.zip')
 
 if __name__ == "__main__":
     show()
