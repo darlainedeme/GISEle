8fd2fcc874b95740cb4b9fc364237f485eea8958
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-07 17:55:53 +0200

    rearrange

--- rearrange.py
+++ rearrange.py
@@ -34,24 +34,18 @@ predefined_files = [
 ]
 
 for file in predefined_files:
-    src = os.path.join("general_input", file)
-    dst = os.path.join("data/predefined/general_input", file)
-    if os.path.exists(src):
-        shutil.move(src, dst)
+    shutil.move(os.path.join("general_input", file), os.path.join("data/predefined/general_input", file))
 
 # Move other predefined files (assuming they are in the root or other known locations)
 # Add any other predefined files here
 
 # Move routing data to data folder
 routing_data_files = [
-    # Add any specific routing data files here if needed
+    # List all routing data files here
 ]
 
 for file in routing_data_files:
-    src = os.path.join("scripts/routing", file)
-    dst = os.path.join("data/predefined", file)
-    if os.path.exists(src):
-        shutil.move(src, dst)
+    shutil.move(os.path.join("scripts/routing", file), os.path.join("data/predefined", file))
 
 # Move scripts to their new locations
 script_files = {
@@ -69,21 +63,11 @@ script_files = {
 }
 
 for src, dst in script_files.items():
-    if os.path.exists(os.path.join("scripts", src)):
-        shutil.move(os.path.join("scripts", src), os.path.join(dst, src))
+    shutil.move(os.path.join("scripts", src), os.path.join(dst, src))
 
 # Move routing scripts to their new location
-routing_scripts_src = "scripts/routing_scripts"
-routing_scripts_dst = "scripts/routing/routing_scripts"
-if os.path.exists(routing_scripts_src):
-    for item in os.listdir(routing_scripts_src):
-        s = os.path.join(routing_scripts_src, item)
-        d = os.path.join(routing_scripts_dst, item)
-        if os.path.isdir(s):
-            shutil.copytree(s, d, dirs_exist_ok=True)
-        else:
-            shutil.copy2(s, d)
-    shutil.rmtree(routing_scripts_src)
+if os.path.exists("scripts/routing_scripts"):
+    shutil.move("scripts/routing_scripts", "scripts/routing/")
 
 # Move utility scripts to utils
 utility_files = [
@@ -92,9 +76,6 @@ utility_files = [
 ]
 
 for file in utility_files:
-    src = os.path.join("scripts", file)
-    dst = os.path.join("utils", file)
-    if os.path.exists(src):
-        shutil.move(src, dst)
+    shutil.move(os.path.join("scripts", file), os.path.join("utils", file))
 
 print("Project files have been rearranged according to the new structure.")
---
+++ structure.py
@@ -0,0 +1,27 @@
+import os
+
+def print_directory_structure(directory, prefix=''):
+    """
+    Prints the directory structure of the given directory.
+
+    :param directory: str, path of the directory to traverse
+    :param prefix: str, prefix for the current level (used for indentation)
+    """
+    try:
+        # List all the entries in the directory
+        entries = os.listdir(directory)
+    except PermissionError:
+        # If we don't have permissions to list directory contents, skip it
+        return
+
+    for entry in entries:
+        path = os.path.join(directory, entry)
+        if os.path.isdir(path):
+            print(f"{prefix}{entry}/")
+            print_directory_structure(path, prefix + '    ')
+        else:
+            print(f"{prefix}{entry}")
+
+# Replace 'your_directory_path' with the path of the directory you want to traverse
+directory_path = r'C:\Users\darla\OneDrive\Roba Vecchia_onedrive\Documenti\GitHub\GISEle'
+print_directory_structure(directory_path)
