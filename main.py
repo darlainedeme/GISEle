96ae117f413e4171012ae8028b43381c641eae02
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-07 17:33:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import streamlit as st
 st.set_page_config(layout="wide")
 
-from scripts import home, area_selection, data_retrieve, buildings, clustering_modes, data_visualization, costs, summary_analysis, demand_estimation, routing, results
-from scripts.routing import routing_preparation, local_area_optimization, create_milp_input, execute_milp, process_milp_output
+from scripts import home, area_selection, data_retrieve, buildings, clustering_modes, data_visualization, costs, summary_analysis, demand_estimation, results, routing_preparation, local_area_optimization
 
 # Define navigation
 main_nav = st.sidebar.radio("Navigation", [
     "Home", "Area Selection", "Data Retrieve", "Buildings", 
     "Clustering", "Data Visualization and Enhancement", 
     "Summary Analysis", "Costs", "Demand Estimation", 
-    "Routing", "Results"
+    "Routing Preparation", "Local Area Optimization", "Results"
 ])
 
 if main_nav == "Home":
@@ -30,20 +29,9 @@ elif main_nav == "Costs":
     costs.show()
 elif main_nav == "Demand Estimation":
     demand_estimation.show()
-elif main_nav == "Routing":
-    routing_nav = st.sidebar.radio("Routing Steps", [
-        "Routing Preparation", "Local Area Optimization", 
-        "Create MILP Input", "Execute MILP", "Process MILP Output"
-    ])
-    if routing_nav == "Routing Preparation":
-        routing_preparation.show()
-    elif routing_nav == "Local Area Optimization":
-        local_area_optimization.show()
-    elif routing_nav == "Create MILP Input":
-        create_milp_input.show()
-    elif routing_nav == "Execute MILP":
-        execute_milp.show()
-    elif routing_nav == "Process MILP Output":
-        process_milp_output.show()
+elif main_nav == "Routing Preparation":
+    routing_preparation.show()
+elif main_nav == "Local Area Optimization":
+    local_area_optimization.show()
 elif main_nav == "Results":
     results.show()
