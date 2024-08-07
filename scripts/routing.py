35bd59bfd1173bb0a2c1d8ab78a872699c044a37
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-07 14:09:35 +0200

    Update routing.py

--- scripts/routing.py
+++ scripts/routing.py
@@ -110,36 +110,13 @@ def set_parameters():
         "simplify_coef": simplify_coef
     }
 
-def run_routing(parameters):
+def clustering_procedure(parameters):
+    st.write('0. Clustering Procedures')
+
     gisele_folder = parameters["gisele_folder"]
     country = parameters["country"]
     case_study = parameters["case_study"]
     crs = parameters["crs"]
-    resolution = parameters["resolution"]
-    voltage = parameters["voltage"]
-    LV_base_cost = parameters["LV_base_cost"]
-    load_capita = parameters["load_capita"]
-    pop_per_household = parameters["pop_per_household"]
-    resolution_population = parameters["resolution_population"]
-    ss_data = parameters["ss_data"]
-    coe = parameters["coe"]
-    grid_lifetime = parameters["grid_lifetime"]
-    landcover_option = parameters["landcover_option"]
-    local_database = parameters["local_database"]
-    MITBuilding = parameters["MITBuilding"]
-    losses = parameters["losses"]
-    reliability_option = parameters["reliability_option"]
-    mg_option = parameters["mg_option"]
-    multi_objective_option = parameters["multi_objective_option"]
-    n_line_type = parameters["n_line_type"]
-    MV_coincidence = parameters["MV_coincidence"]
-    mg_types = parameters["mg_types"]
-    Roads_option = parameters["Roads_option"]
-    Rivers_option = parameters["Rivers_option"]
-    triangulation_logic = parameters["triangulation_logic"]
-    population_dataset_type = parameters["population_dataset_type"]
-
-    st.write('0. Clustering Procedures')
 
     shortProcedureFlag = False
     database = os.path.join(gisele_folder, 'scripts', 'routing_scripts', 'Database')
@@ -152,10 +129,17 @@ def run_routing(parameters):
     except Exception as e:
         st.error(f"Error processing clustering: {e}")
 
-    st.write("Processing completed")
+    st.write("Clustering completed")
+    return output_path_clusters
 
-    
-    # 2- New case study creation
+def new_case_study(parameters, output_path_clusters):
+    st.write("2. New case study creation")
+
+    gisele_folder = parameters["gisele_folder"]
+    country = parameters["country"]
+    case_study = parameters["case_study"]
+    crs = parameters["crs"]
+
     case_study_path = os.path.join('Case studies', case_study)
     if not os.path.exists(case_study_path):
         # Create new folders for the study case
@@ -202,8 +186,7 @@ def run_routing(parameters):
         for i, row in Clusters.iterrows():
             if row['geometry'].geom_type == 'MultiPolygon':
                 Clusters.at[i, 'geometry'] = row['geometry'][0]
-        # Clusters.to_file(os.path.join(case_study_path, 'Input', 'Communities_boundaries'))
-        Clusters.to_file(os.path.join(database, country, 'Input', 'Communities_boundaries'))
+        Clusters.to_file(os.path.join(case_study_path, 'Input', 'Communities_boundaries'))
     else:
         destination_path = os.path.join(database, country, 'Input', 'Communities_boundaries', 'Communities_boundaries.shp')
         st.write(output_path_clusters)
@@ -212,12 +195,23 @@ def run_routing(parameters):
         Clusters = gpd.read_file(destination_path)
         Clusters = Clusters.to_crs(crs)
         Clusters['cluster_ID'] = range(1, Clusters.shape[0] + 1)
-        # st.write(case_study_path)
         study_area = gpd.read_file(os.path.join('scripts', 'routing_scripts', 'Case studies', 'awach555', 'Input', 'Study_area', 'Study_area.shp'))
         Substations = gpd.read_file(os.path.join('scripts', 'routing_scripts', 'Case studies', 'awach555', 'Input', 'substations', 'substations.shp'))
 
-    # Create the grid of points
+    st.write("New case study creation completed")
+    return Clusters, study_area, Substations
+
+def create_weighted_grid(parameters, Clusters, study_area):
     st.write('1. CREATE A WEIGHTED GRID OF POINTS')
+
+    gisele_folder = parameters["gisele_folder"]
+    country = parameters["country"]
+    case_study = parameters["case_study"]
+    crs = parameters["crs"]
+    resolution = parameters["resolution"]
+    resolution_population = parameters["resolution_population"]
+    landcover_option = parameters["landcover_option"]
+
     df_weighted = qgis_process.create_input_csv(crs, resolution, resolution_population, landcover_option, country, case_study, database, study_area)
     accepted_road_types = [
         'living_street', 'pedestrian', 'primary', 'primary_link', 'secondary', 'secondary_link',
@@ -226,15 +220,30 @@ def run_routing(parameters):
     Road_nodes, Road_lines = create_roads_new(gisele_folder, case_study, Clusters, crs, accepted_road_types, resolution, resolution_population)
     Merge_Roads_GridOfPoints(gisele_folder, case_study)
 
-    # Clustering procedure
-    st.write('2. LOCATE SECONDARY SUBSTATIONS INSIDE THE CLUSTERS.')
-    el_th = 0.21
-    Clusters = Clusters[Clusters['elec acces'] < el_th]
-    Clusters['cluster_ID'] = range(1, Clusters.shape[0] + 1)
-    destination_path1 = os.path.join('scripts', 'routing_scripts', 'Case studies', 'awach555', 'Input', 'Communities_boundaries', 'Communities_boundaries_2el.shp')
-    Clusters.to_file(destination_path1)
-    
-    # Optimize local area
+    st.write("Weighted grid of points created")
+    return df_weighted, Road_nodes, Road_lines
+
+def optimize_local_area(parameters, Clusters):
+    st.write('2. Optimize Local Area')
+
+    gisele_folder = parameters["gisele_folder"]
+    country = parameters["country"]
+    resolution_population = parameters["resolution_population"]
+    load_capita = parameters["load_capita"]
+    pop_per_household = parameters["pop_per_household"]
+    road_coef = parameters["road_coef"]
+    case_study = parameters["case_study"]
+    LV_distance = parameters["LV_distance"]
+    ss_data = parameters["ss_data"]
+    landcover_option = parameters["landcover_option"]
+    roads_weight = parameters["roads_weight"]
+    run_genetic = parameters["run_genetic"]
+    max_length_segment = parameters["max_length_segment"]
+    simplify_coef = parameters["simplify_coef"]
+    crit_dist = parameters["crit_dist"]
+    LV_base_cost = parameters["LV_base_cost"]
+    population_dataset_type = parameters["population_dataset_type"]
+
     start = time.time()
     LAO.optimize(
         crs, country, resolution_population, load_capita, pop_per_household, road_coef, Clusters, case_study, LV_distance,
@@ -243,9 +252,28 @@ def run_routing(parameters):
     )
     end = time.time()
     st.write(f"Optimization Time: {end - start}")
-    
-    # Create input for the MILP
+
+def create_MILP_input(parameters, Clusters):
     st.write('5. Create the input for the MILP.')
+
+    gisele_folder = parameters["gisele_folder"]
+    case_study = parameters["case_study"]
+    crs = parameters["crs"]
+    cable_specs = parameters["cable_specs"]
+    resolution = parameters["resolution"]
+    reliability_option = parameters["reliability_option"]
+    Roads_option = parameters["Roads_option"]
+    simplify_road_coef_outside = parameters["simplify_road_coef_outside"]
+    Rivers_option = parameters["Rivers_option"]
+    mg_option = parameters["mg_option"]
+    mg_types = parameters["mg_types"]
+    triangulation_logic = parameters["triangulation_logic"]
+    MV_coincidence = parameters["MV_coincidence"]
+    coe = parameters["coe"]
+    voltage = parameters["voltage"]
+    losses = parameters["losses"]
+    n_line_type = parameters["n_line_type"]
+
     MILP_Input_creation.create_input(
         gisele_folder, case_study, crs, cable_specs['line1']['line_cost'], resolution, reliability_option, Roads_option,
         simplify_road_coef_outside, Rivers_option, mg_option, mg_types, triangulation_logic
@@ -259,9 +287,21 @@ def run_routing(parameters):
         MILP_Input_creation.create_MILP_input_1way(gisele_folder, case_study, crs, mg_option, MV_coincidence)
 
     time.sleep(5)
+    st.write("MILP input created")
 
-    # Execute the MILP model
+def execute_MILP(parameters, n_clusters):
     st.write('6. Execute the MILP according to the selected options.')
+
+    gisele_folder = parameters["gisele_folder"]
+    case_study = parameters["case_study"]
+    coe = parameters["coe"]
+    voltage = parameters["voltage"]
+    cable_specs = parameters["cable_specs"]
+    mg_option = parameters["mg_option"]
+    reliability_option = parameters["reliability_option"]
+    n_line_type = parameters["n_line_type"]
+    losses = parameters["losses"]
+
     start = time.time()
     if not mg_option and reliability_option and n_line_type == 1:
         MILP_models.MILP_without_MG(gisele_folder, case_study, n_clusters, coe, voltage, cable_specs['line1']['resistance'], cable_specs['line1']['reactance'], cable_specs['line1']['Pmax'], cable_specs['line1']['line_cost'])
@@ -281,21 +321,63 @@ def run_routing(parameters):
     end = time.time()
     st.write(f"MILP Execution Time: {end - start}")
 
-    # Process the output from the MILP
+def process_MILP_output(parameters):
     st.write('7. Process MILP output')
+
+    gisele_folder = parameters["gisele_folder"]
+    case_study = parameters["case_study"]
+    crs = parameters["crs"]
+    mg_option = parameters["mg_option"]
+    reliability_option = parameters["reliability_option"]
+    coe = parameters["coe"]
+    n_line_type = parameters["n_line_type"]
+
     process_output.process(gisele_folder, case_study, crs, mg_option, reliability_option)
     process_output.create_final_output(gisele_folder, case_study)
     if mg_option:
         process_output.analyze(gisele_folder, case_study, coe, mg_option, n_line_type)
-    
+
 # Main function
 def show():
     st.title("Routing Procedures")
 
     parameters = set_parameters()
 
-    if st.button("Run Routing"):
-        run_routing(parameters)
+    if st.button("Run Clustering Procedure"):
+        output_path_clusters = clustering_procedure(parameters)
+        st.session_state['output_path_clusters'] = output_path_clusters
+
+    if 'output_path_clusters' in st.session_state:
+        output_path_clusters = st.session_state['output_path_clusters']
+
+        if st.button("Run New Case Study Creation"):
+            Clusters, study_area, Substations = new_case_study(parameters, output_path_clusters)
+            st.session_state['Clusters'] = Clusters
+            st.session_state['study_area'] = study_area
+            st.session_state['Substations'] = Substations
+
+        if 'Clusters' in st.session_state and 'study_area' in st.session_state:
+            Clusters = st.session_state['Clusters']
+            study_area = st.session_state['study_area']
+
+            if st.button("Create Weighted Grid"):
+                df_weighted, Road_nodes, Road_lines = create_weighted_grid(parameters, Clusters, study_area)
+                st.session_state['df_weighted'] = df_weighted
+                st.session_state['Road_nodes'] = Road_nodes
+                st.session_state['Road_lines'] = Road_lines
+
+            if st.button("Optimize Local Area"):
+                optimize_local_area(parameters, Clusters)
+
+            if st.button("Create MILP Input"):
+                create_MILP_input(parameters, Clusters)
+
+            if st.button("Execute MILP"):
+                n_clusters = Clusters.shape[0]
+                execute_MILP(parameters, n_clusters)
+
+            if st.button("Process MILP Output"):
+                process_MILP_output(parameters)
 
 if __name__ == "__main__":
     show()
