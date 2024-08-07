e9319f25c72e90ccbbd2a5fe872530f9517bfb54
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-07 17:25:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 from scripts import home, area_selection, data_retrieve, buildings, clustering_modes, data_visualization, costs, summary_analysis, demand_estimation, routing, results
+from scripts.routing import routing_preparation, local_area_optimization, create_milp_input, execute_milp, process_milp_output
 
 # Define navigation
 main_nav = st.sidebar.radio("Navigation", [
@@ -30,6 +31,19 @@ elif main_nav == "Costs":
 elif main_nav == "Demand Estimation":
     demand_estimation.show()
 elif main_nav == "Routing":
-    routing.show()
+    routing_nav = st.sidebar.radio("Routing Steps", [
+        "Routing Preparation", "Local Area Optimization", 
+        "Create MILP Input", "Execute MILP", "Process MILP Output"
+    ])
+    if routing_nav == "Routing Preparation":
+        routing_preparation.show()
+    elif routing_nav == "Local Area Optimization":
+        local_area_optimization.show()
+    elif routing_nav == "Create MILP Input":
+        create_milp_input.show()
+    elif routing_nav == "Execute MILP":
+        execute_milp.show()
+    elif routing_nav == "Process MILP Output":
+        process_milp_output.show()
 elif main_nav == "Results":
     results.show()
