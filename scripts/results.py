bc103ae58992ebbecedff8a693237079ef5ba028
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-07 19:45:29 +0200

    Update results.py

---
+++ scripts/results.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def show():
+    st.title("Results")
+    st.markdown("## This page is under development 🚧")
+    st.info("Stay tuned! More features will be added soon.")
+
+if __name__ == "__main__":
+    show()
