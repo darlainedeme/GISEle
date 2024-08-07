1ba4eb33aaf5c274db819708a0300389b565c128
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-07 17:33:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 st.set_page_config(layout="wide")
 
-from scripts import home, area_selection, data_retrieve, buildings, clustering_modes, data_visualization, costs, summary_analysis, demand_estimation, results, routing_preparation, local_area_optimization
+from scripts import home, area_selection, data_retrieve, buildings, clustering_modes, data_visualization, costs, summary_analysis, demand_estimation, routing, results
 
 # Define navigation
 main_nav = st.sidebar.radio("Navigation", [
     "Home", "Area Selection", "Data Retrieve", "Buildings", 
     "Clustering", "Data Visualization and Enhancement", 
     "Summary Analysis", "Costs", "Demand Estimation", 
-    "Routing Preparation", "Local Area Optimization", "Results"
+    "Routing", "Results"
 ])
 
 if main_nav == "Home":
@@ -29,9 +29,7 @@ elif main_nav == "Costs":
     costs.show()
 elif main_nav == "Demand Estimation":
     demand_estimation.show()
-elif main_nav == "Routing Preparation":
-    routing_preparation.show()
-elif main_nav == "Local Area Optimization":
-    local_area_optimization.show()
+elif main_nav == "Routing":
+    routing.show()
 elif main_nav == "Results":
     results.show()
