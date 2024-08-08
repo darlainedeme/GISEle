6447374a19e3d862491ddfdd42c68795dc763574
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-08 03:52:34 +0200

    Update elevation.py

--- scripts/data_retrieve_scripts/elevation.py
+++ scripts/data_retrieve_scripts/elevation.py
@@ -5,6 +5,7 @@ import pandas as pd
 import streamlit as st
 import os
 import numpy as np
+from shapely.geometry import Point
 
 def get_elevation_data(locations):
     url = "https://api.ellipsis-drive.com/v3/path/77239b78-ff95-4c30-a90e-0428964a0f00/raster/timestamp/83a6860b-3c34-4a53-9d3f-d123019eff7c/location"
