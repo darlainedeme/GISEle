4df7e85d9ce922b984a446259995296dccfcfe1d
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-09 19:10:27 +0200

    Update case_study_creation.py

--- scripts/gisele_scripts/case_study_creation.py
+++ scripts/gisele_scripts/case_study_creation.py
@@ -114,14 +114,9 @@ def create_input_csv(crs, resolution, resolution_population, landcover_option, d
     st.write(streets.head())
     
     study_area_crs = study_area.to_crs(crs)
-    study_area_crs = study_area.to_crs(crs)
-    st.write("Study area reprojected:")
-    st.write(study_area_crs)
 
     # Create a small buffer to avoid issues
     study_area_buffered = study_area.buffer((resolution * 0.1 / 11250) / 2)
-    st.write("Study area buffered:")
-    st.write(study_area_buffered)
 
     # Clip the protected areas and streets
     protected_areas_clipped = gpd.clip(protected_areas, study_area_crs)
