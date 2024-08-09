2619d5d6200b8f123313d7783a1cb006adde7227
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-09 10:08:17 +0200

    vania

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 import streamlit as st
 
-# Set page configuration at the top level
-st.set_page_config(page_title="Local GISEle", page_icon="🗺️", layout="wide")
-
-from scripts import home, area_selection, data_retrieve, buildings_mapping, clustering_modes, data_visualization, costs, summary_analysis, demand_estimation, routing, results, timelapse
+# Import your other pages
+from scripts import home, area_selection, data_retrieve, buildings_mapping, clustering_modes, data_visualization, costs, summary_analysis, demand_estimation, routing, results, vania
 
 # Display logo at the very top of the sidebar
 st.sidebar.image("data/logo.png", width=200)  # Adjust the width as needed
 
-# Define navigation
+# Define navigation with separators
 main_nav = st.sidebar.radio("Navigation", [
-    "Home", "Area Selection", "Satellite Timelapse", "Data Retrieve", "Data Visualization and Enhancement", 
+    "Home",
+    "_________________________",
+    "Area Selection", 
+    "Data Retrieve", 
+    "_________________________",
     "Buildings", 
-    "Clustering", "Summary Analysis", "Costs", "Demand Estimation", 
-    "Routing", "Results"
+    "Clustering", 
+    "Data Visualization and Enhancement", 
+    "VANIA Report",
+    "_________________________",
+    "Summary Analysis", 
+    "Costs", 
+    "Demand Estimation", 
+    "Routing", 
+    "_________________________",
+    "Results"
 ])
 
 # Navigation dictionary for clean code
 pages = {
     "Home": home.show,
     "Area Selection": area_selection.show,
-    "Satellite Timelapse": timelapse.app,
     "Data Retrieve": data_retrieve.show,
-    "Data Visualization and Enhancement": data_visualization.show,
     "Buildings": buildings_mapping.show,
     "Clustering": clustering_modes.show,
+    "Data Visualization and Enhancement": data_visualization.show,
+    "VANIA Report": vania.show,
     "Summary Analysis": summary_analysis.show,
     "Costs": costs.show,
     "Demand Estimation": demand_estimation.show,
@@ -32,6 +42,6 @@ pages = {
     "Results": results.show
 }
 
-# Display the selected page
-if main_nav in pages:
+# Display the selected page, excluding separators
+if main_nav not in ["_________________________"]:
     pages[main_nav]()
