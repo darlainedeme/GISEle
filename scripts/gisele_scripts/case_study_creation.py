dd08ae75347d3f9fa85cdcf588033b939f26420d
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-09 18:18:51 +0200

    Update case_study_creation.py

--- scripts/gisele_scripts/case_study_creation.py
+++ scripts/gisele_scripts/case_study_creation.py
@@ -89,7 +89,7 @@ def create_input_csv(crs, resolution, resolution_population, landcover_option, d
     """
     crs_str = 'epsg:' + str(crs)
     
-    geospatial_data_path = os.path.join('mount', 'src', 'gisele', 'data', '2_downloaded_input_data')
+    geospatial_data_path = os.path.join('data', '2_downloaded_input_data')
     
     protected_areas_file = os.path.join(geospatial_data_path, 'protected_areas', 'protected_areas.shp')
     roads_file = os.path.join(geospatial_data_path, 'roads', 'roads.shp')
