40125959fbf5cda73d13e874a65d7fdea19f1939
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-12 00:55:14 +0200

    Update optimization.py

--- scripts/gisele_scripts/optimization.py
+++ scripts/gisele_scripts/optimization.py
@@ -204,7 +204,9 @@ def optimize(crs, country, resolution, load_capita, pop_per_household, road_coef
         if population_dataset_type == 'mit':
             grid_of_points = create_grid(crs, resolution, area)
             Population = rasterio.open(os.path.join(dir_input_1, 'population', 'Population.tif'))
-            Population = Population.to_crs(parameters["crs"])
+            # Reproject the grid points to the CRS of the raster before sampling
+            if grid_of_points.crs != Population.crs:
+                grid_of_points = grid_of_points.to_crs(Population.crs)
 
         else:
             grid_of_points = gpd.clip(Population, area_buffered)
