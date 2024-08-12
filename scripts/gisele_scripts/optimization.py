dbd97b5db3409770aa157b1050e43536fed80134
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-08-12 10:59:32 +0200

    Update optimization.py

--- scripts/gisele_scripts/optimization.py
+++ scripts/gisele_scripts/optimization.py
@@ -9,6 +9,7 @@ from rasterio.warp import calculate_default_transform, reproject
 import numpy as np
 from networkx import Graph
 from shapely.ops import split,nearest_points
+import networkx as nx
 
 # Reproject raster utility function
 def reproject_raster(input_raster, dst_crs):
